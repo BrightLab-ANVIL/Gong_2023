2c3259ed01f25d676986f1ea88a5dc86ad0631ba
Jingxuan Gong <jingxuan@u.northwestern.edu>
2022-10-19 10:45:37 -0500

    delete autosync

--- Rapitide_run.py
+++ Rapitide_run.py
@@ -11,7 +11,7 @@ for s in subjects:
         GM_mask_file = "C:/Users/s/Desktop/Data/ATLAS_FILES/GM_0.5/" + s + "_T1_fast_pve_1_GM-MASK50_SBRef.nii.gz"
 
         rapidtide_command = "python C:/Users/s/anaconda3/Scripts/rapidtide" + " " + input_file + " " + output_prefix + " " + \
-             "--globalmeaninclude" + " " + GM_mask_file + ":1 " + "--refineinclude" + " " + GM_mask_file + ":1" + " " + "--delaymapping --autosync --datatstep 1.2 --detrendorder 0 --oversampfac 4 --passes 3 --despecklepasses 4 --filterband lfo --searchrange -15 15 --pickleft --nolimitoutput --spatialfilt -1"
+             "--globalmeaninclude" + " " + GM_mask_file + ":1 " + "--refineinclude" + " " + GM_mask_file + ":1" + " " + "--delaymapping --datatstep 1.2 --detrendorder 0 --oversampfac 4 --passes 3 --despecklepasses 4 --filterband lfo --searchrange -15 15 --pickleft --nolimitoutput --spatialfilt -1"
 
         print(rapidtide_command)
         os.system(rapidtide_command)
